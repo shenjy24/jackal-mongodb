46b3fdd8adc1dc7b27f2525ae04b9f1cab6f6504
shenjy <shenjy24@gmail.com>
2020-09-05 12:24:48 +0800

    add query

--- src/main/java/com/jonas/App.java
+++ src/main/java/com/jonas/App.java
@@ -1,17 +1,7 @@
 package com.jonas;
 
-import com.jonas.data.Game;
-import com.jonas.data.GameManager;
-
-import java.util.UUID;
-
 /**
  * Hello world!
  */
 public class App {
-
-    public static void main(String[] args) {
-        GameManager gameManager = new GameManager();
-
-    }
 }
